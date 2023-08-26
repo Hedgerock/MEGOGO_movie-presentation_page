5ab2e64270f43786a1ba38d697e23c4a31664646
Dmitro Vasyliev <dmitriyvas21@gmail.com>
2023-08-27 00:52:10 +0300

    Global commit, finished an entire page, added mixins, used scss properties such as the element inside of another element

--- scripts/index.js
+++ scripts/index.js
@@ -13,9 +13,16 @@ $(function() {
         $('.header-list_on-click').toggleClass('header-list_visible');
         $('.header-language-sector__title').toggleClass('header-language-sector__title_active');
     });
+    $('.volume-button-off').on('click', function() {
+        $('.volume-button-off').toggleClass('volume-button__onsound');
+    })
     $('.movie-description__more').on('click', function() {
         $('.movie-description__paragraph_invisible').toggleClass('movie-description__paragraph_visible');
     });
+    $('.contacts__button').on('click', function() {
+        $('.contacts-list').toggleClass('contacts-list_visible');
+        $('.contacts__button').toggleClass('contacts__button_active');
+    })
 });
 
 function replace(element){
@@ -24,4 +31,40 @@ function replace(element){
     } else {
       element.textContent = 'подробнее';
     }
+}
+
+const images = document.querySelectorAll('.slider-content-container');
+const sliderBlock = document.querySelector('.slider-content');
+let count = 0;
+let width;
+function init() {
+    width = document.querySelector('.slider-content-container').offsetWidth;
+    sliderBlock.style.width = width*images.length + 'px';
+    images.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    });
+    console.log(width);
+    rollSlider();
+}
+window.addEventListener('resize', init);
+init();
+document.querySelector('.slider-swiper__button_right').addEventListener('click', function() {
+    count++;
+    if(count >= images.length) {
+        count = 0;
+    }
+    rollSlider();
+});
+
+document.querySelector('.slider-swiper__button_left').addEventListener('click', function() {
+    count--;
+    if(count < 0) {
+        count = images.length - 1;
+    }
+    rollSlider();
+});
+
+function rollSlider() {
+    sliderBlock.style.transform = 'translate(-'+count * width + 'px)';
 }
